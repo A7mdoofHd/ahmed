0147a8df3bdad28ec02e60dcfe9b3e7c05667aef
A7mdoofHd <a7med.mads@gmail.com>
2024-09-07 06:27:24 +0300

    Create stayl.js

---
+++ stayl.js
@@ -0,0 +1,158 @@
+$(`<link rel="stylesheet" type="text/css" href="">
+<link rel="stylesheet" href="">
+<style>
+
+.container {
+  display: flex; 
+  
+  align-items: center;
+text-align: center;
+   
+ 
+height: 20px;
+margin-top: 8px;
+margin-bottom: 4px;
+}
+#u1{
+    border-radius: 50px;
+}
+.button1 {
+text-size-adjust: none!important;
+text-size-adjust: auto!important;
+background-image: url("https://www.raed.net/img?id=977277");
+background-size: 100%!important;
+border-bottom:2px solid!important;
+margin-bottom:2px!important;
+border-top:2px solid!important;
+margin-top:2px!important;
+border-color:#bababa!important;
+            color: black;
+margin-top:10px!important;
+margin-bottom:17px!important;
+           
+            border-radius: 30px;
+            height: 25px!important;
+width: auto;
+text-align: center;
+
+}
+.button1:hover {
+   
+    border-radius: 0px;
+    transform: scale(1.1, 1.1)!important;
+    transition : 1s  transform!important;
+    transition : 0.5s  border-radius!important;
+ text-align: justify;
+}
+span.flex-grow-1.dots {text-align: center;}
+.dots {white-space: normal!important;}
+span.btn.fr.fa.fa-eye {transform: matrix(1,0,0,1,-40,-3);}
+.fa.fa-user.label.badgex.label-success {transform: matrix(1,0,0,1,-55,0);}
+div#l1 input#u1,div#l3 button{width:98%!important;margin-left:3px}
+div#l2 button,div#l3 button{ background-image: linear-gradient(to left, rgba(255,255,255,0.2), rgba(99,121,125,15));border-radius:0px 5px 0px 5px!important;margin-left: 3px;border:1px solid #ccc!important;border-radius:2px!important;height:27px!important;padding:0 5px}
+div#l1 input#u1{ opacity: 0.5;height: 25px!important;border-radius:0px 4px 4px 4px!important;border:1px solid #000000;margin-top:3px!important;text-align:center}
+div#l2 input#pass1,div#l2 input#u2,div#l3 input#pass2,div#l3 input#u3{height: 23px!important;width:49%!important;padding:0!important;float:right;margin:2px 5px 2px 0}
+
+div#l2 input#u2,div#l3 input#u3{opacity: 0.5;margin-left:-6px;border-radius:0px 4px 4px 4px!important;border:1px solid #000000;text-align:center}
+div#l2 input#pass1,div#l3 input#pass2{opacity: 0.5;border-radius:0px 4px 4px 4px!important;border:1px solid #000000;text-align:center}
+div#l2,div#l3{padding:4px!important}
+div#l1{margin:0 1px -18px;padding: 4px!important;}
+div#l1,div#l2,div#l3{border-bottom:2px solid;margin-bottom:2px;border-top:2px solid;margin-top:2px;border-color:#bababa;border-radius:15px;height:69px}
+div#l1 button{ background-image: linear-gradient(to left, rgba(255,255,255,0.2), rgba(99,121,125,15));border-radius:0px 5px 0px 5px!important;border:1px solid #ccc!important;border-radius:2px!important;width:98%;height:27px;margin-left: 3px;padding:0 5px}
+div#l2 button{ background-image: linear-gradient(to left, rgba(255,255,255,0.2), rgba(99,121,125,15));border-radius:0px 5px 0px 5px!important;width:70%}
+
+
+ul.nav.nav-tabs>li>a{border:none;text-align:center;border-radius:3px;display:initial;width:80%;float:left;padding:0!important}
+
+ul.nav.nav-tabs>li.active>a{background-image: linear-gradient(to right, rgba(226,223,223,0.8), rgba(255,255,255,20));border-radius:30px;}
+ul.nav.nav-tabs>li{background-image: linear-gradient(to right, rgba(226,223,223,0.8), rgba(255,255,255,20));border-radius:30px!important;width:32%;margin:0 5px 0 0;}
+ul.nav.nav-tabs>li>.fa-user-plus:before,ul.nav.nav-tabs>li>.fa-user:before,ul.nav.nav-tabs>li>.fa-user:before{ background-image: linear-gradient(to right, rgba(255,255,255,0.3), rgba(226,223,223,15));float:right;margin-right:-26%;padding:0 2px 1px;border-radius:8px;width:23px;text-align:center;}
+.nav>li>a:focus,.nav>li>a:hover{color:#3E6778}
+ul.nav.nav-tabs{border-bottom:2px solid;margin-bottom:2px;border-top:2px solid;margin-top:2px;border-color:#bababa!important;background-color:#e2dfdf!important;margin-top:2px;float:right;width:100%;padding:3px 1px;border-radius:15px!important;}
+
+
+iv#tlogins::-webkit-scrollbar{width:0px;height:10px}
+
+div#tlogins .fl.mini.u-msg{margin-top:3px!important;border-radius:3px!important}
+a.label.label-primary.fl img.fl,button.btn.fr.btn-success.fa.fa-refresh,div#l2 br,div#l3 br{display:none}
+
+
+button.btn.fr.btn-success.fa.fa-refresh{display:none}
+a.label.label-primary.fl img.fl{display:none}
+#des22{text-align:center!important;font-size:100%!important}
+.fr.borderg,.fr.borderg a{font-size:10px!important;padding:0 5px!important;position:relative;color:#3E6778}
+.fr.borderg{border:0;margin-top:0;height:0}
+span.s1.fa.fa-user.label.badgex.label-as-badge.label-success{padding:0 3px 2px;margin-right:5px}
+label.label.label-primary.mini.fl{padding:1px;border-radius:3px!important}
+
+a.label.bg.d-flex.fl {border-radius: 3px!important;margin: 1px 2px;padding: 0px!important;margin-bottom: 2px;text-align: right!important;width: 59%!important;}
+ul.nav.nav-tabs {margin-top: 2px;float: right;width: 100%;padding: 3px 1px;height: 30px!important;}
+span.btn.fr.btn-success.fa.fa-refresh {display: none;}
+
+
+
+.sharit{
+background-image: url("https://www.raed.net/img?id=977192");
+background-size: 100%!important;
+border-radius: 20;
+border-bottom:2px solid;margin-bottom:2px;border-top:2px solid;margin-top:2px;border-color:#bababa!important;
+}
+
+</style>`).insertBefore('body')
+
+$(`<img src="https://www.raed.net/img?id=976938" class="" style="width:390px;height:150px;margin-top: 0px;opacity: 0.67;border-radius: 0px 20px 0px 0px;" >
+
+
+<a id="des22" style="height: 29px;width:70px;border-style: dashed #000;float:right;margin:-130px -1.8px 4px;color: #ffffff;transform:matrix(1, 0, 0, 1, -256, 20);background-color: #e55d7b!important; border-radius: 0px 0px 25px 0px;" class="label label label-primary mini fl " target="_blank" class="fl  fa   fa-phone btn btn-warning" href="اشتراكات">إشتراكات</a>
+<a id="des22" style="height: 29px;width:70px;margin:-124.7px -1.8px 2px; border-radius: 0px 25px 0px 0px;float:right;color:rgb(255, 217, 0);transform:matrix(1, 0, 0, 1, -256, -15);" class="label label label-primary mini fl" class="fl  fa   fa-star btn btn-success" target="_blank" href="قوانين">القوانين</a>
+<a id="des22" style="height: 29px;color: rgb(255 255 255);width:60px;margin:-55px -2px 2px; border-radius: 25px 0px 0px 0px;;transform:matrix(1, 0, 0, 1, 6, -85);background-color: #e55d7b!important;" class="label label label-primary mini fl" target="_blank" class="fl  fa  fa-heartbeat btn btn-danger" href="تطبيق">تطبيق</a>
+<a id="des22" style="height: 29px;width:40px;color: rgb(255 217 11);width:60px;;margin:-60.6px 62px 2px; border-radius: 0px 0px 0px 25px;transform:matrix(1, 0, 0, 1, -58, -50);" class="label label label-primary mini fl" target="_blank" class="fl  fa fa-graduation-cap btn btn-info" href="ادارة">اتصل بنا</a>
+
+
+<div class="container">
+
+    <marquee scrolldelay="200" direction="right">
+        <button class="button1" onclick="document.location='https://www.7lla.com/'"> <img src="https://www.raed.net/img?id=977185"width="20" height="18">شات حلا </button>
+
+            
+        <img src="https://www.raed.net/img?id=977185"width="20" height="18">  <button class="button1" onclick="document.location='https://chat-jawlk.com/'" > شات جوالك</button>
+
+        <img src="https://www.raed.net/img?id=977185"width="20" height="18">    <button class="button1" onclick="document.location='https://h3oo.com/'" >شات ايزي</button>
+  
+    <img src="https://www.raed.net/img?id=977185"width="20" height="18">         <button class="button1" onclick="document.location='https://8o9.net/'">شات عربي</button>
+          
+        <img src="https://www.raed.net/img?id=977185"width="20" height="18">     <button class="button1" onclick="document.location='https://flh.chat/'" >شات فلة السعوديه</button>
+
+        <img src="https://www.raed.net/img?id=977185"width="20" height="18">  <button class="button1" onclick="document.location='https://bnt.chat/'" > شات بنت</button>
+
+         <img src="https://www.raed.net/img?id=977185"width="20" height="18">    <button class="button1" onclick="document.location='https://jnaat.com/'" >شات التحليه</button>
+   
+     <img src="https://www.raed.net/img?id=977185"width="20" height="18">         <button class="button1" onclick="document.location='https://q21z.com/'">شات الشمال</button>
+           
+         <img src="https://www.raed.net/img?id=977185"width="20" height="18">     <button class="button1" onclick="document.location='https://7ug1.com/'" >شات قلوبنا</button>
+ 
+         <img src="https://www.raed.net/img?id=977185"width="20" height="18">  <button class="button1" onclick="document.location='https://saudia.chat/'" > شات طويق</button>
+
+         <img src="https://www.raed.net/img?id=977185"width="20" height="18">    <button class="button1" onclick="document.location='https://fkamh.com/'" >شات جيروني</button>
+   
+     <img src="https://www.raed.net/img?id=977185"width="20" height="18">         <button class="button1" onclick="document.location='https://ksa-chat.com/'">شات المملكة السعوديه</button>
+           
+         <img src="https://www.raed.net/img?id=977185"width="20" height="18">     <button class="button1" onclick="document.location='https://skr0.com/'" >شات سكر</button>
+ 
+         <img src="https://www.raed.net/img?id=977185"width="20" height="18">  <button class="button1" onclick="document.location='https://chat-altaif.com/'" > شات فلة الاصدقاء</button>
+
+         <img src="https://www.raed.net/img?id=977185"width="20" height="18">    <button class="button1" onclick="document.location='https://ch1tt.net/'" >شات سهر</button>
+   
+     <img src="https://www.raed.net/img?id=977185"width="20" height="18">         <button class="button1" onclick="document.location='https://fhh0.com/'">شات فهاوة</button>
+        
+       <img src="https://www.raed.net/img?id=977185"width="20" height="18">       <button class="button1" onclick="document.location='https://www.jawalchat.net/'" style="font-size: 10px!important;">شات جوال<img src="https://www.raed.net/img?id=977185"width="20" height="18"></button>
+  
+   
+</marquee>
+</div>
+
+`).insertBefore('.nav-tabs');
+
+
+$(`<marquee direction="right" width="100%" class="sharit" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="4" style="text-align: center;height: 23px;color: #10384f;font-family: jazeera,FontAwesome;margin-right: -1px;margin-top: 5px;border-radius: 30px;width: 100%;padding: 1px;height:28px;"><img src="https://www.raed.net/img?id=977185"width="20" height="18" class="inta"> في عصر التواصل الحديث عبر الإنترنت، أصبحت وسائل التواصل الاجتماعي جزءًا لا يتجزأ من حياتنا اليومية. يلجأ الكثيرون إلى  الدردشة للتواصل مع الأصدقاء ، وللتعبير عن أفكارهم ومشاعرهم ، سنتحدث عن شات "المملكة" وكيف يمكن أن يكون مكانًا مثاليًا لبدء محادثات مميزة والتواصل مع الأشخاص الذين تحبهم. فـ مرحباً بكم ،   <img src="https://www.raed.net/img?id=977185"width="20" height="">
+</marquee></div>`).insertBefore('.bg.mini.fl');
