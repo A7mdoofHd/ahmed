50e138ed73715d502da5dc626c73db298506739e
A7mdoofHd <a7med.mads@gmail.com>
2024-09-08 12:06:12 +0300

    Create toktok.js

---
+++ toktok.js
@@ -0,0 +1,7 @@
+setInterval(() => {
+  const ahmed = ahmed;
+  myid != null && ahmed[ahmed(363)](ahmed => {
+    const ahmed = ahmed, ahmed = $(users).find(".uzr:contains('" + ahmed[ahmed(442)] + "')");
+    ahmed && !ahmed.hasClass(ahmed[ahmed(435)]) && ($(users)[ahmed(415)](ahmed(427) + ahmed[ahmed(442)] + "')").addClass(ahmed.cls), $(users)[ahmed(415)](ahmed(397) + ahmed[ahmed(435)] + ahmed(410))[ahmed(436)](ahmed(430) + ahmed[ahmed(455)] + ahmed(354) + ahmed[ahmed(465)] + '">'));
+  });
+}, 6e3);
