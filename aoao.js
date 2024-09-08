aa813e4e286e50b4ee78d7dd3d1a6a56ecd5573f
A7mdoofHd <a7med.mads@gmail.com>
2024-09-08 09:50:31 +0300

    Create aoao.js

---
+++ aoao.js
@@ -0,0 +1,6 @@
+ const divElement = document.getElementById('myDiv');
+        const username = 'Ahmed'; // افترض أن المستخدم هو إداري
+
+        if (username === 'YASSER') {
+            divElement.classList.add('user-admin');
+        }
